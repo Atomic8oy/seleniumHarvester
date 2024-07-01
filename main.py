8cb8eff2ef707ee47c03ef73592196288e1fd08f
Atomic8oy <tpaix87@gmail.com>
2024-07-01 08:53:05 +0330

    added an error message for invalid website

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from config import WEBSITE
+from utilities import log
 
 if WEBSITE == "digikala":
     import digikala
 elif WEBSITE == "divar":
     import divar
 else:
-    pass
+    log("Website variable in config file is not valid.\nPlease read the readme.md")
