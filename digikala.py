57ec96fcb4b0bafef8b2ffc2234f2a9b10fe1b46
Atomic8oy <tpaix87@gmail.com>
2024-07-03 10:51:00 +0330

    Added scrolling to digikala

--- digikala.py
+++ digikala.py
@@ -2,8 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from json import dumps
 
-from utilities import convertDigits, log
-from config import KEYWORD, OUT
+from utilities import convertDigits, log, exists
+from config import KEYWORD, OUT, SCROLL, SCROLL_AMOUNT
 
 log("Creating the webdriver")
 browser = webdriver.Chrome()
@@ -13,27 +13,34 @@ log(f"Heading to [https://www.digikala.com/search/?q={KEYWORD}]")
 browser.get(f"https://www.digikala.com/search/?q={KEYWORD}")
 log("[DONE]", True)
 
-browser.implicitly_wait(30.0)
+browser.implicitly_wait(10.0)
 
-log("Getting price elements")
-pth = "//*[@id='ProductListPagesWrapper']/section[1]/div[2]/div/a/div/article/div[2]/div[2]/div[4]/div[1]/div/span"
-elements = browser.find_elements(By.XPATH, pth)
-log("[DONE]", True)
+for x in range(SCROLL):
+    log("Getting price elements")
+    pth = "//*[@id='ProductListPagesWrapper']/section[1]/div[2]/div/a/div/article/div[2]/div[2]/div[4]/div[1]/div/span"
+    elements = browser.find_elements(By.XPATH, pth)
+    log("[DONE]", True)
 
 
-log("Harvesting and converting data")
-items = {}
-special = False
-id = 0
-for elm in elements:
-    if "٪" in elm.text:
-        special = True
-        continue
-    price = convertDigits(elm.text.replace(",", ""))
-    items.update({id : {"price": price, "isSpecial": special}})
+    log("Harvesting and converting data")
+    items = {}
     special = False
-    id += 1
-log("[DONE]", True)
+    id = 0
+    for elm in elements:
+        if "٪" in elm.text:
+            special = True
+            continue
+        price = convertDigits(elm.text.replace(",", ""))
+
+        item = {"price": price, "isSpecial": special}
+        if not exists(item, items):
+            items.update({id : item})
+            id += 1
+        special = False
+        
+    log("[DONE]", True)
+
+    browser.execute_script(f"window.scrollBy(0,{SCROLL_AMOUNT})")
 
 log(f"Writing output data in out/{OUT}.json")
 with open(f"out/{OUT}.json", 'w') as file:
