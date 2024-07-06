66c2e8749606dab5c1b41e79e3657c2edfb7e658
Atomic8oy <tpaix87@gmail.com>
2024-07-06 10:57:20 +0330

    Added a log for excel thing

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ else:
     log("Website variable in config file is not valid.\nPlease read the readme.md")
 
 if EXCEL:
+    log(f"Saving as excel in [out/{OUT}.json]")
     from config import OUT
     import pandas
-
+    
     pandas.read_json(f"out/{OUT}.json").to_excel(f"out/{OUT}.xlsx")
+    log("[DONE]")
