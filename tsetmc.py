3bece5bc24ad69d0263c45fb8502b57fc8747fe3
Atomic8oy <tpaix87@gmail.com>
2024-07-13 11:03:39 +0330

    testmc support completed

--- tsetmc.py
+++ tsetmc.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from json import dumps
 
@@ -9,10 +9,10 @@ from utilities import log
 
 driver = webdriver.Chrome()
 
-driver.implicitly_wait(20.0)
+driver.implicitly_wait(5.0)
 
-log(f"Heading to [https://www.tsetmc.com/History/65883838195688438/20240709]")
-driver.get(f"https://www.tsetmc.com/History/65883838195688438/20240709")
+log(f"Heading to [https://www.tsetmc.com/History/{KEYWORD}]")
+driver.get(f"https://www.tsetmc.com/History/{KEYWORD}")
 log("[DONE]", True)
 
 log("Doing the range bar thing")
@@ -23,19 +23,27 @@ for x in range(540, 862):
     slider.send_keys(Keys.ARROW_RIGHT)
 log("[DONE]", True)
 
+driver.implicitly_wait(0.0)
+
 items = []
 for x in range(540, 862):
     log(f"Reading {int(x/60)}:{x%60}")
-    pth = "/html/body/div/div/div[2]/div[2]/div[2]/div[1]/div[4]/div[1]/table/tr[1]"
+    pth = '/html/body/div/div/div[2]/div[2]/div[2]/div[1]/div[4]/div[1]/table/tr[1]'
     elm = driver.find_element(By.XPATH, pth)
-    price = elm.text.replace("آخرین معامله ", "")
-    price = price.replace("(", "")
-    price = price.replace(")", "")
-    price = price.replace(",", "")
-    price = price.split(" ")
-    items.append({"time": x ,"price": price[0]})
+    price = elm.text
+    price = price.replace("آخرین معامله ", "")
+    try:
+        change = elm.find_element(By.XPATH, '//*[@id="d02"]/span').text
+        price = price.replace(change, "")
+    except NoSuchElementException:
+        change = None
+    items.append({"time": x ,"price": price, "change": change})
     log("[DONE]", True)
     slider.send_keys(Keys.ARROW_LEFT)
 
+log(f"Writing the result in [out/{OUT}.json]")
 with open(f"out/{OUT}.json", 'w') as file:
     file.write(dumps(items))
+log("[DONE]", True)
+
+driver.quit()
